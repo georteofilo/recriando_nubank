c9cc0f7f5753fa7aa81f21385760d704b953dbdc
George Rocha <georteofilo.dev@gmail.com>
2023-06-29 11:42:54 -0300

    atualizando Balance e home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -4,11 +4,40 @@ import { StyleSheet, Text, View } from 'react-native';
 import Header from '../../components/Header';
 import Balance from '../../components/Balance';
 
+const list = [
+    {
+        id: 1,
+        label: 'Boleto conta luz',
+        value: '300,90',
+        date: '17/09/2022',
+        type: 0 //despesas
+    },
+    {
+        id: 2,
+        label: 'Pix Cliente X',
+        value: '1610,00',
+        date: '17/09/2022',
+        type: 1, // entradas
+    },
+    {
+        id: 3,
+        label: 'Salário',
+        value: '2700,00',
+        date: '17/09/2022',
+        type: 1, // entradas
+    }
+]
+
+
+
 export default function Home() {
   return (
     <View style={styles.container}>
         <Header name="George Rocha"/>
-        <Balance />
+
+        <Balance saldo="8.970,15" gastos="-6.345,87" />
+
+        <Text style={styles.title}>Últimas movimentações</Text>
     </View>
   );
 }
@@ -16,6 +45,12 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fafafa',
   },
+  title:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 14,
+    marginRight: 14,
+  }
 });
--- src/components/Balance/index.js
+++ src/components/Balance/index.js
@@ -5,16 +5,63 @@ import {
     StyleSheet
 } from 'react-native';
 
-export default function Balance() {
+export default function Balance({saldo, gastos}) {
  return (
    <View style={styles.container}>
-    <Text>TESTE COMPONENTE</Text>
+
+        <View style={styles.item}>
+            <Text style={styles.itemTitle}>Saldo</Text>
+                <View style={styles.content}>
+                    <Text style={styles.currencySymbol}>R$</Text>
+                    <Text style={styles.balance}>{saldo}</Text>
+                </View>
+        </View>
+
+        <View style={styles.item}>
+            <Text style={styles.itemTitle}>Gastos</Text>
+                <View style={styles.content}>
+                    <Text style={styles.currencySymbol}>R$</Text>
+                    <Text style={styles.expanses}>{gastos}</Text>
+                </View>
+        </View>
+
    </View>
   );
 }
 
 const styles = StyleSheet.create({
     container:{
-        
+        backgroundColor: '#fff',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingStart: 18,
+        paddingEnd: 18,
+        marginTop: -26,
+        marginStart: 14,
+        marginEnd: 14,
+        borderRadius: 4,
+        paddingTop: 22,
+        paddingBottom: 22,
+        zIndex: 99,
+    },
+    itemTitle:{
+        fontSize: 20,
+        color: '#dadada',
+    },
+    content:{
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    currencySymbol:{
+        color: '#dadada',
+        marginRight: 6,
+    },
+    balance:{
+        fontSize: 22,
+        color: '#2ecc71',
+    },
+    expanses:{
+        fontSize: 22,
+        color: '#e74c3c',
     }
 })
